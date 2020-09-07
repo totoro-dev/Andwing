db0141b5f4c2383b1cc40848f0cb458b31f1fe8a
totoro-dev <2844150336@qq.com>
2020-09-07 12:23:41 +0800

    [Fix][解决ImageView和ImageButton的继承结构]

--- src/main/java/top/totoro/swing/widget/view/ImageButton.java
+++ src/main/java/top/totoro/swing/widget/view/ImageButton.java
@@ -1,138 +1,25 @@
 package top.totoro.swing.widget.view;
 
-import top.totoro.swing.widget.base.BaseAttribute;
-import top.totoro.swing.widget.bean.ViewAttribute;
-import top.totoro.swing.widget.util.Log;
+import java.util.Objects;
 
-import javax.swing.*;
-import java.awt.*;
-import java.net.URL;
+import static top.totoro.swing.widget.util.AttributeDefaultValue.scaleFitCenter;
 
-import static top.totoro.swing.widget.util.AttributeDefaultValue.*;
-
-public class ImageButton extends View<ViewAttribute, JPanel> {
+public class ImageButton extends ImageView {
 
     private final String TAG = getClass().getSimpleName();
 
-    protected final JLabel mImageContainer;
-    protected ImageIcon imageIcon;
-    protected int width = -3, height = -3;
-    protected String scaleType = scaleFitCenter;
-
     public ImageButton(View parent) {
         super(parent);
-        component = new JPanel(null);
-        mImageContainer = new JLabel("", JLabel.CENTER) {
-            @Override
-            public void paint(Graphics g) {
-                switch (scaleType) {
-                    default:
-                    case scaleFitCenter:
-                        fitCenter();
-                    case scaleCenter:
-                        // 居中显示
-                        mImageContainer.setLocation((component.getWidth() - width) / 2, (component.getHeight() - height) / 2);
-                        break;
-                    case scaleFitXY:
-                        fitXY();
-                        mImageContainer.setLocation(0, 0);
-                        break;
-                    case scaleFitStart:
-                        fitCenter();
-                        mImageContainer.setLocation(0, 0);
-                        break;
-                    case scaleFitEnd:
-                        fitCenter();
-                        mImageContainer.setLocation(component.getWidth() - width, component.getHeight() - height);
-                        break;
-                }
-                super.paint(g);
-            }
-        };
-        component.add(mImageContainer);
-    }
-
-    private void fitCenter() {
-        if (attribute.getWidth() != BaseAttribute.WRAP_CONTENT
-                || attribute.getHeight() != BaseAttribute.WRAP_CONTENT) {
-            float widthScale = component.getWidth() / (float) width;
-            float heightScale = component.getHeight() / (float) height;
-            float scale = Math.min(widthScale, heightScale);
-            if ((widthScale < 1 || heightScale < 1) && scale != 1) {
-                Log.d(TAG, "scale = " + scale);
-                width *= scale;
-                height *= scale;
-                imageIcon.setImage(imageIcon.getImage().getScaledInstance(width, height, Image.SCALE_FAST));
-                mImageContainer.setSize(width, height);
-            }
-        }
-    }
-
-    protected void fitXY() {
-    }
-
-    protected void fitStart() {
-    }
-
-    public void setImage(String src, boolean invalidate) {
-        if (src == null || "".equals(src)) {
-            Log.e(TAG, "为id为" + attribute.getId() + "的" + TAG + "设置背景图片时，图片路径不能为空");
-            return;
-        }
-        URL url = getClass().getClassLoader().getResource(src);
-        if (url != null) {
-            attribute.setSrc(src);
-            imageIcon = new ImageIcon(url);
-        } else {
-            Log.e(TAG, "为id为" + attribute.getId() + "的" + TAG + "设置背景图片时，图片路径不正确");
-            imageIcon = null;
-        }
-        mImageContainer.setIcon(imageIcon);
-        if (invalidate) {
-            invalidateSuper();
-        } else {
-            reSizeAsImageSize();
-        }
-    }
-
-    public void setImage(String src) {
-        setImage(src, true);
     }
 
     @Override
-    public void setAttribute(ViewAttribute attribute) {
-        super.setAttribute(attribute);
-        // 背景透明
-        component.setOpaque(false);
-        setImage(attribute.getSrc(), false);
-    }
-
-    @Override
-    public void invalidate() {
-        super.invalidate();
-        reSizeAsImageSize();
-    }
-
-    /**
-     * 根据宽高属性的wrap或确定的值，重置图片大小
-     * 该方法一般在开始整体布局大小确定前调用
-     */
-    private void reSizeAsImageSize() {
-        if (imageIcon == null) {
-            width = height = 0;
-        } else {
-            width = imageIcon.getIconWidth();
-            height = imageIcon.getIconHeight();
+    public void setScaleType(String scaleType, boolean invalidateParent) {
+        if (!Objects.equals(this.scaleType, scaleFitCenter)) {
+            /* ImageButton的图像必须保持fitCenter */
+            this.scaleType = scaleFitCenter;
         }
-        if (attribute.getWidth() == BaseAttribute.WRAP_CONTENT) {
-            setMinWidth(width);
-        }
-        if (attribute.getHeight() == BaseAttribute.WRAP_CONTENT) {
-            setMinHeight(height);
-        }
-        mImageContainer.setSize(width, height);
-        // 具体的位置只有在绘制的时候才能确定
-//        mImageContainer.setLocation((component.getWidth() - width) / 2, (component.getHeight() - height) / 2);
+        // 触发图片的刷新，并决定是否刷新父布局
+        setImage(getAttribute().getSrc(), invalidateParent);
     }
 
 }
--- src/main/java/top/totoro/swing/widget/view/ImageView.java
+++ src/main/java/top/totoro/swing/widget/view/ImageView.java
@@ -5,13 +5,23 @@ import org.dom4j.Element;
 import top.totoro.swing.widget.base.BaseAttribute;
 import top.totoro.swing.widget.bean.ViewAttribute;
 import top.totoro.swing.widget.util.AttributeKey;
+import top.totoro.swing.widget.util.Log;
 
+import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
 import java.util.Objects;
 
 import static top.totoro.swing.widget.util.AttributeDefaultValue.*;
 
-public class ImageView extends ImageButton {
+public class ImageView extends View<ViewAttribute, JPanel> {
+
+    private final String TAG = getClass().getSimpleName();
+
+    private final JLabel mImageContainer;
+    private ImageIcon imageIcon;
+    private int width = -3, height = -3;
+    protected String scaleType = scaleFitCenter;
 
     public static final String FIT_CENTER = scaleFitCenter;
     public static final String CENTER = scaleCenter;
@@ -21,28 +31,110 @@ public class ImageView extends ImageButton {
 
     public ImageView(View parent) {
         super(parent);
+        component = new JPanel(null);
+        mImageContainer = new JLabel("", JLabel.CENTER) {
+            @Override
+            public void paint(Graphics g) {
+                switch (scaleType) {
+                    default:
+                    case scaleFitCenter:
+                        fitCenter();
+                    case scaleCenter:
+                        // 居中显示
+                        mImageContainer.setLocation((component.getWidth() - width) / 2, (component.getHeight() - height) / 2);
+                        break;
+                    case scaleFitXY:
+                        fitXY(); // 拉伸
+                        // 满控件显示
+                        mImageContainer.setLocation(0, 0);
+                        break;
+                    case scaleFitStart:
+                        fitCenter();
+                        // 左上显示
+                        mImageContainer.setLocation(0, 0);
+                        break;
+                    case scaleFitEnd:
+                        fitCenter();
+                        // 右下显示
+                        mImageContainer.setLocation(component.getWidth() - width, component.getHeight() - height);
+                        break;
+                }
+                super.paint(g);
+            }
+        };
+        component.add(mImageContainer);
+    }
+
+    private void fitCenter() {
+        if (attribute.getWidth() != BaseAttribute.WRAP_CONTENT
+                || attribute.getHeight() != BaseAttribute.WRAP_CONTENT) {
+            float widthScale = component.getWidth() / (float) width;
+            float heightScale = component.getHeight() / (float) height;
+            float scale = Math.min(widthScale, heightScale);
+            if ((widthScale < 1 || heightScale < 1) && scale != 1) {
+                Log.d(TAG, "scale = " + scale);
+                width *= scale;
+                height *= scale;
+                imageIcon.setImage(imageIcon.getImage().getScaledInstance(width, height, Image.SCALE_FAST));
+                mImageContainer.setSize(width, height);
+            }
+        }
+    }
+
+    public void setImage(String src, boolean invalidateParent) {
+        if (src == null || "".equals(src)) {
+            Log.e(TAG, "为id为" + attribute.getId() + "的" + TAG + "设置背景图片时，图片路径不能为空");
+            return;
+        }
+        URL url = getClass().getClassLoader().getResource(src);
+        if (url != null) {
+            attribute.setSrc(src);
+            imageIcon = new ImageIcon(url);
+        } else {
+            Log.e(TAG, "为id为" + attribute.getId() + "的" + TAG + "设置背景图片时，图片路径不正确");
+            imageIcon = null;
+        }
+        mImageContainer.setIcon(imageIcon);
+        if (invalidateParent) {
+            invalidateSuper();
+        } else {
+            reSizeAsImageSize();
+        }
+    }
+
+    public void setImage(String src) {
+        setImage(src, true);
     }
 
     @Override
     public void setAttribute(ViewAttribute attribute) {
         super.setAttribute(attribute);
+        // 背景透明
+        component.setOpaque(false);
         // 获取设置的缩放类型
         Element element = attribute.getElement();
         Attribute scaleType = element.attribute(AttributeKey.scaleType);
         if (scaleType != null) {
-            this.scaleType = scaleType.getValue();
+            setScaleType(scaleType.getValue(), false);
+        } else {
+            setImage(attribute.getSrc(), false);
         }
     }
 
     public void setScaleType(String scaleType) {
-        if (Objects.equals(this.scaleType, scaleType)) return;
-        this.scaleType = scaleType;
-        // 触发图片的刷新
-        setImage(getAttribute().getSrc());
+        // 全局刷新
+        setScaleType(scaleType, true);
     }
 
-    @Override
-    protected void fitXY() {
+    public void setScaleType(String scaleType, boolean invalidateParent) {
+        if (!Objects.equals(this.scaleType, scaleType)) {
+            this.scaleType = scaleType;
+        }
+        // 触发图片的刷新，并决定是否刷新父布局
+        setImage(getAttribute().getSrc(), invalidateParent);
+    }
+
+    private void fitXY() {
         if (getAttribute().getWidth() != BaseAttribute.WRAP_CONTENT) {
             width = component.getWidth();
         }
@@ -51,6 +143,34 @@ public class ImageView extends ImageButton {
         }
         imageIcon.setImage(imageIcon.getImage().getScaledInstance(width, height, Image.SCALE_FAST));
         mImageContainer.setSize(width, height);
+    }
+
+    @Override
+    public void invalidate() {
+        super.invalidate();
+        reSizeAsImageSize();
+    }
+
+    /**
+     * 根据宽高属性的wrap或确定的值，重置图片大小
+     * 该方法一般在开始整体布局大小确定前调用
+     */
+    private void reSizeAsImageSize() {
+        if (imageIcon == null) {
+            width = height = 0;
+        } else {
+            width = imageIcon.getIconWidth();
+            height = imageIcon.getIconHeight();
+        }
+        if (attribute.getWidth() == BaseAttribute.WRAP_CONTENT) {
+            setMinWidth(width);
+        }
+        if (attribute.getHeight() == BaseAttribute.WRAP_CONTENT) {
+            setMinHeight(height);
+        }
+        mImageContainer.setSize(width, height);
+        // 具体的位置只有在绘制的时候才能确定
+//        mImageContainer.setLocation((component.getWidth() - width) / 2, (component.getHeight() - height) / 2);
     }
 
 }
